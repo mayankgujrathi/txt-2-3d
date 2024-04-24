62175b92d1a6ebab61a5f4ba1d0348d8181c8262
mayank <mayankgujrathi@gmail.com>
2024-04-24 15:09:48 +0530

    updated: model_cache

--- mod/prebuild/download.py
+++ mod/prebuild/download.py
@@ -1,7 +1,3 @@
-"""
-Adapted from: https://github.com/openai/glide-text2im/blob/69b530740eb6cef69442d6180579ef5ba9ef063e/glide_text2im/download.py
-"""
-
 import hashlib
 import os
 from functools import lru_cache
@@ -14,32 +10,19 @@ from filelock import FileLock
 from tqdm.auto import tqdm
 
 MODEL_PATHS = {
-    "transmitter": "https://openaipublic.azureedge.net/main/shap-e/transmitter.pt",
-    "decoder": "https://openaipublic.azureedge.net/main/shap-e/vector_decoder.pt",
-    "text300M": "https://openaipublic.azureedge.net/main/shap-e/text_cond.pt",
-    "image300M": "https://openaipublic.azureedge.net/main/shap-e/image_cond.pt",
+    "transmitter": "transmitter.pt",
+    "decoder": "vector_decoder.pt",
+    "text300M": "text_cond.pt",
+    "image300M": "image_cond.pt",
 }
 
 CONFIG_PATHS = {
-    "transmitter": "https://openaipublic.azureedge.net/main/shap-e/transmitter_config.yaml",
-    "decoder": "https://openaipublic.azureedge.net/main/shap-e/vector_decoder_config.yaml",
-    "text300M": "https://openaipublic.azureedge.net/main/shap-e/text_cond_config.yaml",
-    "image300M": "https://openaipublic.azureedge.net/main/shap-e/image_cond_config.yaml",
-    "diffusion": "https://openaipublic.azureedge.net/main/shap-e/diffusion_config.yaml",
+    "transmitter": "transmitter_config.yaml",
+    "decoder": "vector_decoder_config.yaml",
+    "text300M": "text_cond_config.yaml",
+    "image300M": "image_cond_config.yaml",
+    "diffusion": "diffusion_config.yaml",
 }
-
-URL_HASHES = {
-    "https://openaipublic.azureedge.net/main/shap-e/transmitter.pt": "af02a0b85a8abdfb3919584b63c540ba175f6ad4790f574a7fef4617e5acdc3b",
-    "https://openaipublic.azureedge.net/main/shap-e/vector_decoder.pt": "d7e7ebbfe3780499ae89b2da5e7c1354012dba5a6abfe295bed42f25c3be1b98",
-    "https://openaipublic.azureedge.net/main/shap-e/text_cond.pt": "e6b4fa599a7b3c3b16c222d5f5fe56f9db9289ff0b6575fbe5c11bc97106aad4",
-    "https://openaipublic.azureedge.net/main/shap-e/image_cond.pt": "cb8072c64bbbcf6910488814d212227de5db291780d4ea99c6152f9346cf12aa",
-    "https://openaipublic.azureedge.net/main/shap-e/transmitter_config.yaml": "ffe1bcb405104a37d9408391182ab118a4ef313c391e07689684f1f62071605e",
-    "https://openaipublic.azureedge.net/main/shap-e/vector_decoder_config.yaml": "e6d373649f8e24d85925f4674b9ac41c57aba5f60e42cde6d10f87381326365c",
-    "https://openaipublic.azureedge.net/main/shap-e/text_cond_config.yaml": "f290beeea3d3e9ff15db01bde5382b6e549e463060c0744f89c049505be246c1",
-    "https://openaipublic.azureedge.net/main/shap-e/image_cond_config.yaml": "4e0745605a533c543c72add803a78d233e2a6401e0abfa0cad58afb4d74ad0b0",
-    "https://openaipublic.azureedge.net/main/shap-e/diffusion_config.yaml": "efcb2cd7ee545b2d27223979d41857802448143990572a42645cd09c2942ed57",
-}
-
 
 @lru_cache()
 def default_cache_dir() -> str:
@@ -49,58 +32,11 @@ def default_cache_dir() -> str:
 def fetch_file_cached(
     url: str, progress: bool = True, cache_dir: Optional[str] = None, chunk_size: int = 4096
 ) -> str:
-    """
-    Download the file at the given URL into a local file and return the path.
-    If cache_dir is specified, it will be used to download the files.
-    Otherwise, default_cache_dir() is used.
-    """
-    expected_hash = URL_HASHES[url]
-
-    if cache_dir is None:
-        cache_dir = default_cache_dir()
+    cache_dir = default_cache_dir()
     os.makedirs(cache_dir, exist_ok=True)
-    local_path = os.path.join(cache_dir, url.split("/")[-1])
+    local_path = os.path.join(cache_dir, url)
     if os.path.exists(local_path):
-        check_hash(local_path, expected_hash)
         return local_path
-
-    response = requests.get(url, stream=True)
-    size = int(response.headers.get("content-length", "0"))
-    with FileLock(local_path + ".lock"):
-        if progress:
-            pbar = tqdm(total=size, unit="iB", unit_scale=True)
-        tmp_path = local_path + ".tmp"
-        with open(tmp_path, "wb") as f:
-            for chunk in response.iter_content(chunk_size):
-                if progress:
-                    pbar.update(len(chunk))
-                f.write(chunk)
-        os.rename(tmp_path, local_path)
-        if progress:
-            pbar.close()
-        check_hash(local_path, expected_hash)
-        return local_path
-
-
-def check_hash(path: str, expected_hash: str):
-    actual_hash = hash_file(path)
-    if actual_hash != expected_hash:
-        raise RuntimeError(
-            f"The file {path} should have hash {expected_hash} but has {actual_hash}. "
-            "Try deleting it and running this call again."
-        )
-
-
-def hash_file(path: str) -> str:
-    sha256_hash = hashlib.sha256()
-    with open(path, "rb") as file:
-        while True:
-            data = file.read(4096)
-            if not len(data):
-                break
-            sha256_hash.update(data)
-    return sha256_hash.hexdigest()
-
 
 def load_config(
     config_name: str,
